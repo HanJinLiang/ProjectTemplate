b6c7a18ec7cc105e6a955b2be1fb51d17afc928a
HanJinLiang <hanjinliang1992@163.com>
2018-04-12 18:05:15 +0800

    Initial commit

---
+++ app/src/main/java/com/hanjinliang/projecttemplate/module/main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hanjinliang.projecttemplate.module.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hanjinliang.projecttemplate.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
